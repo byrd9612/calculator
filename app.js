f334f21d82cdd867f64b9563da1ec712824d1eaf
“Kody <158676132+byrd9612@users.noreply.github.com>
2024-03-22 07:14:43 -0500

    finalcode

---
+++ app.js
@@ -0,0 +1,16 @@
+
+const display = document.querySelector('.display')
+
+function appendToDisplay(input) {
+    display.value += input;
+}
+
+
+function clearDisplay() {
+    display.value = ""
+}
+
+function calculate() {
+    display.value = eval(display.value)
+}
+
